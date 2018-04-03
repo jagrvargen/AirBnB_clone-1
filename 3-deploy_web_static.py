3c048e91283cf668a1e13db1daf9710a0fd33e7b
jagrvargen <jagarthewolf@gmail.com>
2018-04-03 05:24:01 +0000

    moved deploy function to bottom in order to prevent two calls to do_pack

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,15 +1,65 @@
 #!/usr/bin/python3
 """
-   This module contains a Fabric script which creates and distributes an
-   archive to two web servers.
+   This module contains the functions, deploy, do_pack, and do_deploy
 """
 from fabric.api import *
-do_pack = __import__("1-pack_web_static").do_pack
-do_deploy = __import__("2-do_deploy_web_static").do_deploy
+from datetime import datetime
+from pathlib import Path
 
 env.hosts = ["52.200.24.63", "54.144.25.175"]
 env.user = "ubuntu"
 
+
+def do_pack():
+    timestamp = datetime.utcnow().strftime("%Y%m%d%H%M%S")
+    archive = "web_static_" + timestamp + ".tgz"
+
+    local("mkdir -p versions")
+    local("tar -cvzf versions/{} web_static/".format(archive))
+
+    my_file = Path("versions/{}".format(archive))
+    if my_file.is_file():
+        return my_file
+    else:
+        return None
+
+def do_deploy(archive_path):
+    if not archive_path:
+        return False
+
+    path_name = archive_path[:-4]
+    file_name = archive_path[9:]
+
+    try:
+        # Change to the tmp directory and upload the .tgz file.
+        put(archive_path, "/tmp/{}".format(file_name))
+
+        # Create the directory to archive the .tgz file into.
+        sudo("mkdir -p /data/web_static/releases/{}/".format(path_name))
+
+        # Uncompress the zip file into the directory.
+        sudo("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(
+            file_name, path_name))
+
+        # Delete the archive
+        sudo("rm  /tmp/{}".format(file_name))
+
+        # Move all previous static content to new directory and remove old one.
+        sudo("mv /data/web_static/releases/{}/web_static/* \
+        /data/web_static/releases/{}/".format(path_name, path_name))
+        sudo("rm -rf /data/web_static/releases/{}/web_static".format(
+            path_name))
+
+        # Delete old symbolic link and create a new one.
+        sudo("rm -rf /data/web_static/current")
+        sudo("ln -s /data/web_static/releases/{}/ /data/web_static/current"
+            .format(path_name))
+
+        print("New version deployed!")
+        return True
+
+    except:
+        return False
 
 def deploy():
     # Call the do_pack function which creates a tar file from the
@@ -23,6 +73,5 @@ def deploy():
     # Call the do_deploy function which deploys the .tgz archive to
     # two web servers and return its value (True or False).
     archive_path = str(archive)
-    result = do_deploy(archive_path)
-
-    return result
+    env.hosts = ["52.200.24.63", "54.144.25.175"]
+    return do_deploy(archive_path)
